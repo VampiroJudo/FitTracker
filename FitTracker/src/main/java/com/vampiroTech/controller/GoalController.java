d537600eae9167115639d66a793532af5130eba8
John <johnconley507@hotmail.com>
2017-08-15 20:20:15 -0400

    adding new html and css for errors

--- FitTracker/src/main/java/com/vampiroTech/controller/GoalController.java
+++ FitTracker/src/main/java/com/vampiroTech/controller/GoalController.java
@@ -29,9 +29,13 @@ public class GoalController {
 	@RequestMapping(value = "addGoal", method = RequestMethod.POST)
 	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
 		
-		
+		System.out.println("result has errors: " + result.hasErrors());
 		
 		System.out.println("Minutes updates" +goal.getMinutes());
+		
+		if(result.hasErrors()) {
+			return "addGoals";
+		}
 		
 		return "redirect:addMinutes.html";
 	}
