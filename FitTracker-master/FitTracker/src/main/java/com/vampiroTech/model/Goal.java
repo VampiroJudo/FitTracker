f979bbdd6653d6aba338d56d529d7bfc6ffe37d3
John <johnconley507@hotmail.com>
2017-09-28 16:55:46 -0400

    installing persistence annotations

--- FitTracker-master/FitTracker/src/main/java/com/vampiroTech/model/Goal.java
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/model/Goal.java
@@ -1,15 +1,31 @@
 package com.vampiroTech.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
 public class Goal {
 	
+	private Long id;
+
 	@Range(min = 1, max = 120)
 	private int minutes;
+	
+	@Id;
+	@GeneratedValue;
+	public Long getId() {
+		return id;
+	}
 
 	public int getMinutes() {
 		return minutes;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(int minutes) {
