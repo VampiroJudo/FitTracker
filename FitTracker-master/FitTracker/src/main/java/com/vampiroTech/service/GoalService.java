7d21136d3d0a62572773cd0c4940262722807414
John <johnconley507@hotmail.com>
2017-09-29 19:41:27 -0400

    finishing GoalServiceImpl

--- FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalService.java
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalService.java
@@ -1,5 +1,9 @@
 package com.vampiroTech.service;
 
+import com.vampiroTech.model.Goal;
+
 public interface GoalService {
+	
+	Goal save(Goal goal);
 
 }
---
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalServiceImpl.java
@@ -0,0 +1,14 @@
+package com.vampiroTech.service;
+
+import org.springframework.stereotype.Service;
+
+import com.vampiroTech.model.Goal;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+	public Goal save(Goal goal) {
+		return null;
+	}
+
+}
