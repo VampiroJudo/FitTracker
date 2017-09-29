2ec60e548f024a1333adcdb1539263dfc5cca20c
John <johnconley507@hotmail.com>
2017-09-29 19:54:08 -0400

    creating Goal Respository

---
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/repository/GoalRepository.java
@@ -0,0 +1,9 @@
+package com.vampiroTech.repository;
+
+import com.vampiroTech.model.Goal;
+
+public interface GoalRepository {
+	
+	Goal save(Goal goal);
+
+}
---
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/repository/GoalRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.vampiroTech.repository;
+
+import com.vampiroTech.model.Goal;
+import com.vampiroTech.service.GoalService;
+
+public class GoalRepositoryImpl implements GoalService {
+
+	public Goal save(Goal goal) {
+		return null;
+	}
+
+}
