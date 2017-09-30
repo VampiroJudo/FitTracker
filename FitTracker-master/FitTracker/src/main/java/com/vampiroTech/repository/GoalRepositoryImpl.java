a48088ea6bfbcae02d94517aec5f03bda383a5c5
John <johnconley507@hotmail.com>
2017-09-29 20:03:03 -0400

    configuring repository

--- FitTracker-master/FitTracker/src/main/java/com/vampiroTech/repository/GoalRepositoryImpl.java
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/repository/GoalRepositoryImpl.java
@@ -1,11 +1,20 @@
 package com.vampiroTech.repository;
 
+import org.springframework.stereotype.Repository;
+
 import com.vampiroTech.model.Goal;
-import com.vampiroTech.service.GoalService;
 
-public class GoalRepositoryImpl implements GoalService {
+@Repository("goalRepository")
+public class GoalRepositoryImpl implements GoalRepository {
 
+	
+	@PersistenceContext
+	private EntityManager em;
+	
 	public Goal save(Goal goal) {
+		
+		em.persist(goal);
+		
 		return null;
 	}
 
--- FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalServiceImpl.java
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalServiceImpl.java
@@ -3,12 +3,19 @@ package com.vampiroTech.service;
 import org.springframework.stereotype.Service;
 
 import com.vampiroTech.model.Goal;
+import com.vampiroTech.repository.GoalRepository;
 
 @Service("goalService")
 public class GoalServiceImpl implements GoalService {
 
 	public Goal save(Goal goal) {
-		return null;
+		
+		@Autowired
+		private GoalRepository goalRespository;
+		
+		return goalRepository.save(goal);
+		
+		
 	}
 
 }
