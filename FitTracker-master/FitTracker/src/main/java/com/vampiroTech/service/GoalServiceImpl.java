0c39bc88489dbac27d43143467289991dd515a56
John <johnconley507@hotmail.com>
2017-09-29 20:03:58 -0400

    things are getting 'buggy'

--- FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalServiceImpl.java
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/service/GoalServiceImpl.java
@@ -1,5 +1,6 @@
 package com.vampiroTech.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vampiroTech.model.Goal;
