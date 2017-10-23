56d24e3c2e38d98026d15d1f45979a5ba6216e41
John <johnconley507@hotmail.com>
2017-10-23 16:59:36 -0400

    adding constructors for projection

--- FitTracker-master/FitTracker/src/main/java/com/vampiroTech/model/GoalReport.java
+++ FitTracker-master/FitTracker/src/main/java/com/vampiroTech/model/GoalReport.java
@@ -3,26 +3,33 @@ package com.vampiroTech.model;
 public class GoalReport {
 	
 	private int goalMinutes;
-	public int getGoalMinutes() {
-		return goalMinutes;
-	}
-	public void setGoalMinutes(int goalMinutes) {
+	private int exerciseMinutes;
+	private String exerciseActivity;
+	
+	public GoalReport(int goalMinutes, int exerciseMinutes, String exerciseActivity) {
 		this.goalMinutes = goalMinutes;
+		this.exerciseMinutes = exerciseMinutes;
+		this.exerciseActivity = exerciseActivity;
+	}
+	
+	public String getExerciseActivity() {
+		return exerciseActivity;
 	}
 	public int getExerciseMinutes() {
 		return exerciseMinutes;
 	}
-	public void setExerciseMinutes(int exerciseMinutes) {
-		this.exerciseMinutes = exerciseMinutes;
-	}
-	public String getExerciseActivity() {
-		return exerciseActivity;
+	public int getGoalMinutes() {
+		return goalMinutes;
 	}
 	public void setExerciseActivity(String exerciseActivity) {
 		this.exerciseActivity = exerciseActivity;
 	}
-	private int exerciseMinutes;
-	private String exerciseActivity;
+	public void setExerciseMinutes(int exerciseMinutes) {
+		this.exerciseMinutes = exerciseMinutes;
+	}
+	public void setGoalMinutes(int goalMinutes) {
+		this.goalMinutes = goalMinutes;
+	}
 	
 
 }
